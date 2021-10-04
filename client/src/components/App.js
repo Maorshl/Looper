a6b18b9b83c85768c2d7f1c6505e61256d9e3696
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-10-04 14:28:33 +0300

    Added buttons to loops and onClick event listener

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,52 @@
 import "../style/App.css";
+import Looper from "../Looper";
+import { Button, ButtonGroup } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const MainLoop = new Looper();
+  const [button1, setButton1] = useState(false);
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Button
+        variant={button1 ? "secondary" : "primary"}
+        onClick={() => {
+          MainLoop.addLoop("_1");
+        }}
+      >
+        Funk
+      </Button>
+      <Button
+        onClick={() => {
+          MainLoop.addLoop("_2");
+        }}
+      >
+        Bass
+      </Button>
+      <Button
+        onClick={() => {
+          MainLoop.addLoop("_3");
+        }}
+      >
+        Strutter
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => {
+          MainLoop.start();
+        }}
+      >
+        start
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => {
+          MainLoop.stop();
+        }}
+      >
+        stop
+      </Button>
     </div>
   );
 }
