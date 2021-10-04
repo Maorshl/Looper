48167edb2770b060ce96ee79eb77af58f94822ad
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-10-04 15:43:29 +0300

    Created loop button.

---
+++ client/src/components/LoopButton.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+function LoopButton({ mainLoop, songName }) {
+  const [isPressed, setIsPressed] = useState(false);
+  return (
+    <Button
+      variant={isPressed ? "danger" : "success"}
+      onClick={() => {
+        mainLoop.addLoop(songName);
+        setIsPressed((prevState) => !prevState);
+      }}
+    >
+      {songName}
+    </Button>
+  );
+}
+
+export default LoopButton;
