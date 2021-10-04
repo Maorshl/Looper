fa4d6dc234e8d462cf2a2690dcf9e8a2262748f6
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-10-04 23:15:15 +0300

    Update LoopButton.js

--- client/src/components/LoopButton.js
+++ client/src/components/LoopButton.js
@@ -5,6 +5,7 @@ function LoopButton({ mainLoop, songName }) {
   const [isPressed, setIsPressed] = useState(false);
   return (
     <Button
+      className="loop-button"
       variant={isPressed ? "danger" : "success"}
       onClick={() => {
         mainLoop.addLoop(songName);
