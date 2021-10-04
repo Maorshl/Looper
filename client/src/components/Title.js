763f79ec805df30706c88c53c80b27edb26f9607
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-10-05 00:08:24 +0300

    Created a title component

---
+++ client/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import monkey from "../style/images/monkey.png";
+function Title(props) {
+  return (
+    <h1>
+      Dance <img src={monkey} />
+      onkey
+    </h1>
+  );
+}
+
+export default Title;
